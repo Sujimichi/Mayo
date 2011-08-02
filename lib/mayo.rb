d7159c8e37934a9ee467ba5358264a204eaf6406
sujimichi <sujimichi@googlemail.com>
2011-08-02 13:08:36 +0100

    proof of concept with the ability to send different sets of instructions to different clients and have them perform the task.  No return of results as yet.

--- lib/mayo.rb
+++ lib/mayo.rb
@@ -8,7 +8,6 @@ module Mayo
   end
 end
 
-
 class Mayo::Server
   require 'socket'
   require 'dalli'
@@ -20,7 +19,7 @@ class Mayo::Server
 
   def self.run
     @cache = Dalli::Client.new('localhost:11211')   
-    @cache.set("mayo_instruction", "go nuts")
+    @cache.set("mayo_instruction", "run")
   end
 
   def self.stop
@@ -33,61 +32,88 @@ class Mayo::Server
   def initialize
     puts "Initializing Mayo Server - The Rich Creamy Goodness of your tests will soon be spread."
     @cache = Dalli::Client.new('localhost:11211')   
-    @project_dir = Dir.getwd
-    
+    @project_dir = Dir.getwd   
     @clients = []
-    @active_clients = []
-    @threads = []
-
-    @threads << Thread.new { wait_for_clients }
-
-    @cache.set("mayo_instruction", nil)
-    order = nil
-    last_order = nil
-    puts "waiting for orders"     
-    while order != "stop"
-      order = @cache.get("mayo_instruction")
-      @cache.set("mayo_instruction", nil)
-      if order && order != last_order
-        puts "got order: #{order}"
-        last_order = order
-      end
-    end
-    self.stop
+    @threads = [ Thread.new { wait_for_clients } ]  #Create a background thread which accepts connections from new clients
+    listen_for_intructions #listen for instrunctions sent via memcached 
   end
 
-  def do_stuff
-    #@cache.set("active_mayo_server", self)
-    puts "sleeping"
-    sleep 5
+  def wait_for_clients
+    server = TCPServer.open(2000)
+    puts "Ready to accept clients"
+    loop {
+      client = server.accept
+      client.puts({:project_dir => @project_dir, :servername => Socket.gethostname}.to_json) #Send data to client       
+      client_data = client.gets # Read info from client       
+      client_data = JSON.parse(client_data).merge!("socket" => client)
+      @clients << client_data
+      puts "Signing up client: #{client_data['name']}"       
+    }
+  end
 
-    puts current_clients.inspect
-    instruct_clients current_clients
+  def listen_for_intructions
+    @cache.set("mayo_instruction", nil) #make sure no old instruction
+    puts "waiting for orders"     
+    while @cache.get("mayo_instruction").nil?
+    end
+    self.perform(@cache.get("mayo_instruction"))
+    @cache.set("mayo_instruction", nil)     #reset cache
+  end
+
+  def perform order
+    puts "got order: #{order}"
+    case order
+    when "run"
+      run_features
+    when "stop"
+      return self.stop
+    end
+    listen_for_intructions
+  end
+
+  def run_specs
+    update_active_clients
+    specs = Dir['spec/**/*spec.rb']
+    specs = ["spec/lib/vr_xml_acceptance_spec.rb", "spec/models/entity_spec.rb"]
+
+    active_clients do |client, index|
+      command = "bundle exec rspec #{specs[index]}"
+      client["socket"].puts({:run => command}.to_json)
+    end
+  end
+
+  def run_features
+    update_active_clients
+    specs = Dir['features/**/*.feature']
+
+    active_clients do |client, index|
+      command = "bundle exec cucumber features/support/ features/step_definitions/ #{specs[index]}"
+      client["socket"].puts({:run => command}.to_json)
+    end
+
+  end
+
+
+  def update_active_clients
+    print "\nUpdating Active Clients' Data"
+    c = 0
+    active_clients do |client|
+      print(".")
+      client["socket"].puts({:display => "receiving files"}.to_json)
+      send_files_to_client client
+      client["socket"].puts("goto_project_dir")
+      c += 1
+    end
+    puts "updated #{c} clients"
   end
 
   def stop
     puts "Shutting down"
-    @threads.each{|thread| thread.kill}
-  end
-
-  def wait_for_clients
-    server = TCPServer.open(2000)   
-    
-    loop {
-      print "."
-      #Thread.start(server.accept) do |client|
-        client = server.accept
-        client.puts({:project_dir => @project_dir}.to_json) #Send data to client       
-        client_data = client.gets # Read info from client       
-        client_data = JSON.parse(client_data)
-        client_data["socket"] = client
-        @clients << client_data
-        puts "Signing up new client: #{client_data['name']}"       
-        send_files_to_client(client_data)
-
-        #client.close                
-      #end
+    active_clients{|c| 
+      c["socket"].puts({:display => "got kill message"}.to_json) 
+      c["socket"].close
     }
+    @threads.each{|thread| thread.kill}
   end
 
   def send_files_to_client client_data
@@ -95,26 +121,22 @@ class Mayo::Server
     `#{command}`
   end
 
+  def active_clients &blk
+    current_clients.each_with_index do |client, index|
+      yield(client, index)      
+    end
+  end
+
+  #Quick and Dirty Discovery of which clients are still connected and alive
   def current_clients
-    @clients.select{|c| 
+    @clients.select{|c| #Select the clients which respond
       begin  
-        s = c["socket"]
-        s.puts("respond")
-        s.gets.chomp.eql?("alive")
+        c["socket"].puts("respond") #If the client has disconected this line will throw exception
+        true
       rescue
         false
       end
     }
-  end
-
-  def instruct_clients clients
-    instructions = %w[party do_action]
-    clients.each_with_index do |c, i|
-      c = c["socket"]
-      c.puts "fuck you"
-      c.puts instructions[i]
-      c.close
-    end
   end
 end
 
@@ -146,7 +168,11 @@ class Mayo::Client
     @socket.puts(@client_data.to_json)
     @server_inf = JSON.parse(@socket.gets)
     #@socket.close
-    @project_dir = @server_inf["project_dir"]
+    @project_name = @server_inf["project_dir"].split("/").last
+    @project_dir = Dir.getwd + "/" + @project_name
+
+    puts "Registered with Mayo Server #{@server_inf["servername"]} for project #{@project_name}"
+    
     order = true
     while !order.nil?
       begin
@@ -172,8 +198,22 @@ class Mayo::Client
     when "do_action"
       command = "gedit"
       system command
+    when "goto_project_dir"
+      Dir.chdir(@project_dir)
+      puts "now in #{Dir.getwd}"
     else
-      puts "WHAT? Server is talking rubbish \"#{order}\""
+      begin
+        order = JSON.parse(order)
+        if order.keys[0].eql?("display")
+          puts order[order.keys[0]]
+        elsif order.keys[0].eql?("run")
+          result = `#{order[order.keys[0]]}`
+          puts result
+          @socket.puts(result)
+        end
+      rescue
+        puts "WHAT? Server is talking rubbish - \"#{order}\""
+      end
     end
   end
 
--- spec/mayo_spec.rb
+++ spec/mayo_spec.rb
@@ -4,5 +4,23 @@ describe Mayo do
 
   it 'should have tests' 
 
+  describe Mayo::Server do 
+
+    it 'should wait for clients to connect and then have a list of clients' do 
+      s = Mayo::Server.new
+      s.clients.should be empty?
+
+      sleep 2
+      c1 = Mayo::Client.new
+      c2 = Mayo::Client.new
+      c1.should_receive(:client_name).once.and_return("jim")
+      c2.should_receive(:client_name).once.and_return("jan")
+
+      s.clients.size.should == 2
+
+
+    end
+
+  end
 
 end
